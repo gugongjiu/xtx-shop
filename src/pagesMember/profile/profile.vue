cef3e76926c8551c4646f0eb91d662f559cb2e8d
Gugongjiu <1611598378@qq.com>
2024-01-19 23:36:14 +0800

    feat: 会员中心-个人信息 头像更新

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { getMemberProfileApi } from '@/services/profile'
+import { useMemberStore } from '@/stores'
 import type { ProfileDetail } from '@/types/member'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
@@ -11,9 +12,42 @@ const getMemberProfileData = async () => {
   const res = await getMemberProfileApi()
   profile.value = res.result
 }
+const memberStore = useMemberStore()
 onLoad(() => {
   getMemberProfileData()
 })
+
+// 更新用户头像
+const onAvatarChange = () => {
+  uni.chooseMedia({
+    count: 1,
+    mediaType: ['image'],
+    success: (res) => {
+      const { tempFilePath } = res.tempFiles[0]
+      uni.uploadFile({
+        url: '/member/profile/avatar',
+        name: 'file',
+        filePath: tempFilePath,
+        success: (res) => {
+          if (res.statusCode === 200) {
+            const { avatar } = JSON.parse(res.data).result
+            profile.value!.avatar = avatar
+            memberStore.profile!.avatar = avatar
+            uni.showToast({
+              icon: 'success',
+              title: '修改成功!',
+            })
+          } else {
+            uni.showToast({
+              icon: 'error',
+              title: '出错了!',
+            })
+          }
+        },
+      })
+    },
+  })
+}
 </script>
 
 <template>
@@ -25,7 +59,7 @@ onLoad(() => {
     </view>
     <!-- 头像 -->
     <view class="avatar">
-      <view class="avatar-content">
+      <view class="avatar-content" @tap="onAvatarChange">
         <image class="image" :src="profile?.avatar" mode="aspectFill" />
         <text class="text">点击修改头像</text>
       </view>
